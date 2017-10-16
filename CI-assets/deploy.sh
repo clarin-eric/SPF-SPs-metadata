af2475b7035037276bb6e7c8a7bc675ed85b6091
André Moreira <andre@clarin.eu>
2017-10-16 17:14:35 +0200

    Setting up QA report generation via Travis

--- CI-assets/deploy.sh
+++ CI-assets/deploy.sh
@@ -57,7 +57,7 @@ ssh-add ../CI-assets/deploy_key
 
 ls -Fla ..
 pwd
-mv source_md_about_spf_sps* ../ && rm -rf * && mv ../source_md_about_spf_sps* . 
+mv source_md_about_spf_sps* ../ && rm -rf * && rm -rf .* && mv ../source_md_about_spf_sps* . 
 ls -Fla ..
 pwd
 
