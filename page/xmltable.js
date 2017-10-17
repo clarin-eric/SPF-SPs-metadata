ea2ffc96b46482cb9319b3de3b70d9f5ffb6a059
André Moreira <andre@clarin.eu>
2017-10-17 17:13:00 +0200

    Sort table entries by entityID

--- page/xmltable.js
+++ page/xmltable.js
@@ -1,33 +1,56 @@
+
+// compare function for sorting
+function compare(a,b) {
+  if (a.children[0].textContent < b.children[0].textContent)
+     return -1;
+  if (a.children[0].textContent > b.children[0].textContent)
+    return 1;
+  return 0;
+}
+
+function parse(node) {
+    if (node.nodeType != 1) return null;
+    var result = {textContent: node.textContent};
+    var children = [];
+    for (var i = 0; i < node.childNodes.length; i++) {
+        var child = parse(node.childNodes[i]);
+        if (child)
+        {
+            children.push(child);
+        }
+    }    
+    result.children = children;
+    return result;
+}
+
 function loadXMLDoc() {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
+      console.log(parse(this.responseXML.children[0]));
+      generateHTML(parse(this.responseXML.children[0]));
     }
   };
   xmlhttp.open("GET", "../master_sps_qa_report_results.xml", true);
   xmlhttp.setRequestHeader("Content-Type", "text/xml");
   xmlhttp.send();
 }
-function myFunction(xml) {
+
+function generateHTML(xml) {
   var i;
-  var xmlDoc = xml.responseXML;
-  var table="<tr><th>entityID</th><th>Explanation</th><th>Requirement</th></tr>";
-  var x = xmlDoc.getElementsByTagName("result");
-  for (i = 0; i <x.length; i++) { 
-    requirement = x[i].getElementsByTagName("requirement")[0].childNodes[0].nodeValue;
+  x = xml.children.sort(compare);;
+  var table="<tr><th>entityID</th><th>Issue</th><th>Requirement explanation</th></tr>";
+  for (i = 0; i < x.length; i++) { 
+    requirement = x[i].children[2].textContent;
     colorClass = "yellow-row";
     if (requirement.indexOf("Completely a requirement") !== -1) {
         colorClass = "red-row";
     }
-    enityID = x[i].getElementsByTagName("sp")[0].childNodes[0].nodeValue;
+    enityID = x[i].children[0].textContent;
     table += "<tr class=" + colorClass + "><td><a href='" + enityID +"'>" +
-    enityID +
-    "</a></td><td>" +
-    x[i].getElementsByTagName("explanation")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    requirement +
-    "</td></tr>";
+      enityID + "</a></td><td>" +
+      x[i].children[1].textContent +
+      "</td><td>" + requirement + "</td></tr>";
   }
   document.getElementById("report").innerHTML = table;
 }
