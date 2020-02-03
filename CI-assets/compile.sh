9d4ad9a51c0c3ae44af801a0e8bb3100b18c6ae5
andmor <amoreira.mail@gmail.com>
2020-02-03 17:41:52 +0100

    One file per SP

--- CI-assets/compile.sh
+++ CI-assets/compile.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
-QA_VALIDATOR_VERSION=1.0.7
+QA_VALIDATOR_VERSION=1.0.8
 SAXON_VERSION=SaxonHE9-9-1-5J
 SAXON_URL=https://netcologne.dl.sourceforge.net/project/saxon/Saxon-HE/9.9/$SAXON_VERSION.zip
 SCHEMATRON_VERSION=1.0.1-e16ecc4-CLARIN
 INSTALLS_PATH=qa-tmp
 
+sed_cmd="sed"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	sed_cmd="gsed"
+fi
 
 mkdir -p $INSTALLS_PATH/saxon
 cd $INSTALLS_PATH/saxon
@@ -21,12 +25,41 @@ rm -rf schematron-$SCHEMATRON_VERSION schematron.tar.gz
 wget -O SAML_metadata_QA_validator.tar.gz https://codeload.github.com/clarin-eric/SAML_metadata_QA_validator/tar.gz/$QA_VALIDATOR_VERSION
 tar xvf SAML_metadata_QA_validator.tar.gz
 cd SAML_metadata_QA_validator-$QA_VALIDATOR_VERSION
-ant -v
-rm out/SAML_metadata_QA_validator.concrete.sch out/SAML_metadata_QA_validator.xsl out/.gitignore
-if [ ! -d "../../out/" ]; then
-	mkdir ../../out/
+md_files=../../metadata/*.xml
+for file in ${md_files}
+do
+    ant -v -DinputFile="file:$(realpath ${file})"
+done
+number_of_files=$(ls ${md_files} 2> /dev/null | wc -l)
+if [ ${number_of_files} -gt 1 ]; then
+	echo "Generating and testing aggregated output..."
+	((xmllint -xpath "/*[local-name()='EntitiesDescriptor' and namespace-uri()='urn:oasis:names:tc:SAML:2.0:metadata']" ../../CI-assets/feed_wrapper.xml  | \
+        head -1; xmllint -xpath "/*[local-name()='EntityDescriptor' and namespace-uri()='urn:oasis:names:tc:SAML:2.0:metadata']" ../../metadata/*;tail -1 ../../CI-assets/feed_wrapper.xml) | \
+        xmllint --nsclean --format -) > ../../$TARGET_BRANCH/aggregated_feed_${SOURCE_BRANCH}.xml
+	ant -v -DinputFile="file:$(realpath ../../$TARGET_BRANCH/aggregated_feed_${SOURCE_BRANCH}.xml)"
 fi
-mv out/* ../../out/reports
+rm -rf out/*.sch out/*.xsl out/.gitignore out/aggregated_feed_${SOURCE_BRANCH}.xml
+if [ ! -d "../../$TARGET_BRANCH/reports/" ]; then
+    mkdir ../../$TARGET_BRANCH/reports/
+fi
+for report in out/*results.xml
+do
+	set +e
+    if ! diff -q report ../../$TARGET_BRANCH/reports/$(basename ${report}) > /dev/null 2>&1; then
+    	set -e
+    	echo "Report $(basename report) has changed"
+
+    	${sed_cmd} -i "2i <report>" ${report}
+    	${sed_cmd} -i "3i <FromCommit>${SHA}</FromCommit>" ${report}
+    	${sed_cmd} -i "\$a</report>" ${report}
+    	
+    	mv ${report} ../../$TARGET_BRANCH/reports/
+    	filename_wo_ext="${report%_results.xml}"
+    	mv ${filename_wo_ext}.xml ../../$TARGET_BRANCH/reports/
+    	mv ${filename_wo_ext}.svrlt ../../$TARGET_BRANCH/reports/
+    else
+    	set -e
+    	echo "Report $(basename ${report}) is unhanged. Leaving previous version in place."
+    fi
+done
 cd ../.. && rm -rf $INSTALLS_PATH
-
-
--- CI-assets/deploy.sh
+++ CI-assets/deploy.sh
@@ -3,9 +3,10 @@ set -e # Exit with nonzero exit code if anything fails
 
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
+COMMIT_AUTHOR_EMAIL="clarin_bot@clarin.eu"
 
 function doCompile {
-  CI-assets/compile.sh
+  . CI-assets/compile.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -15,30 +16,29 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 # Save some useful information
-REPO=`git config remote.origin.url`
+REPO=$(git config remote.origin.url)
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
+SHA=$(git rev-parse --verify HEAD)
 
 # Clone the existing qa-output for this repo into out/
-# Create a new empty branch if qa-output doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+# Create a new empty branch if qa-output doesn't exist yet (should only happen on first deploy)
+rm -rf $TARGET_BRANCH
+git clone $REPO $TARGET_BRANCH
+cd $TARGET_BRANCH
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean out existing output
-rm -rf out/*.xml out/*.svrlt || exit 0
 
 # Run our compile script
+cd ..
 doCompile
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd $TARGET_BRANCH
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
+git add -A .
+if git diff $TARGET_BRANCH --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
 fi
@@ -54,14 +54,18 @@ chmod 600 ../CI-assets/deploy_key
 eval `ssh-agent -s`
 ssh-add ../CI-assets/deploy_key
 
-# Clean everything else but the build output
-mv .git ../git_tmp && mv .travis.yml README.md web reports ../ && cd .. && rm -rf out && mkdir out && mv .travis.yml README.md web reports out && mv git_tmp out/.git && cd out
-
-
 # Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
-git add -A .
 git commit -m "Deploy SAML QA report for: ${SHA}"
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
+
+# Comment pull request
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Commenting pull request"
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+        -d "{\"body\": \"https://img.shields.io/github/status/s/pulls/clarin-eric/SPF-SPs-metadata/${TRAVIS_PULL_REQUEST}
+Automated QA assessment complete. Please check you SP at [QA report](https://clarin-eric.github.io/SPF-SPs-metadata/page/qa_reports_index.html)\"}" \
+        "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
+exit 0
