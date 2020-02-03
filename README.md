8108d886efe8fae27212ea9eb5f9516ce7e43f5b
Travis CI <clarin_bot@clarin.eu>
2020-02-03 11:21:48 +0100

    New bootstrap based report QA page

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 This branch contains automatically generated QA reports after each commit/merge of SAML metadata into the *master* branch.
 The reports are generated in respect to the [clarin-sp-metadata.xml](https://github.com/clarin-eric/SPF-SPs-metadata/blob/master/clarin-sp-metadata.xml) file of the *master* branch.
 
-You can visualize the most recent report rendered [here](https://clarin-eric.github.io/SPF-SPs-metadata/page/master_qa_report.html).
+You can visualize the most recent report rendered [here](https://clarin-eric.github.io/SPF-SPs-metadata/web/master_qa_report.html).
 
 New reports will be available as soon as the travis CI build succeeds, after the pull request has been merged into the *master* branch. Check for the latest commit to this branch to see if you have an updated report.
---
+++ web/xmltable.js
@@ -0,0 +1,73 @@
+
+// compare function for sorting
+function compare(a,b) {
+  if (a.children[0].textContent < b.children[0].textContent)
+     return -1;
+  if (a.children[0].textContent > b.children[0].textContent)
+    return 1;
+  if (a.children[2].textContent.indexOf("Completely a requirement") !== -1 && b.children[2].textContent.indexOf("Completely a requirement") === -1)
+    return -1;
+  if (a.children[2].textContent.indexOf("Completely a requirement") === -1 && b.children[2].textContent.indexOf("Completely a requirement") !== -1)
+    return 1;
+  return 0;
+}
+
+function parse(node) {
+    if (node.nodeType != 1) return null;
+    var result = {textContent: node.textContent};
+    var children = [];
+    for (var i = 0; i < node.childNodes.length; i++) {
+        var child = parse(node.childNodes[i]);
+        if (child)
+        {
+            children.push(child);
+        }
+    }    
+    result.children = children;
+    return result;
+}
+
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      //console.log(parse(this.responseXML.children[0]));
+      generateHTML(parse(this.responseXML.children[0]), this);
+    }
+  };
+  xmlhttp.open("GET", encodeURI("../reports/master_sps_qa_report_results.xml"), true);
+  xmlhttp.setRequestHeader("Content-Type", "text/xml");
+  xmlhttp.send();
+
+}
+
+function generateHTML(xml, response) {
+  $("#lastModified").append(response.getResponseHeader("Last-Modified"));
+  $("#reportTable").append("<thead class='thead-dark'><tr class='d-flex'><th class='col-2' scope='col'>entityID</th><th class='col-6' scope='col'>Issue</th><th class='col-4' scope='col'>Requirement explanation</th></tr></thead>");
+  $("#reportTable").append("<tbody id='QAtableBody'>");
+  var i;
+  x = xml.children.sort(compare);
+  for (i = 0; i < x.length; i++) { 
+    requirement = x[i].children[2].textContent;
+    colorClass = "table-warning";
+    if (requirement.indexOf("Completely a requirement") !== -1) {
+        colorClass = "table-danger";
+    }
+    enityID = x[i].children[0].textContent;
+    $("#QAtableBody").append("<tr class='" + colorClass + " d-flex'><th class='col-2 text-break' scope='row'><a href='" + enityID +"'>" +
+      enityID + "</a></th><td class='col-6 text-break'>" +
+      x[i].children[1].textContent +
+      "</td><td class='col-4 text-break'>" + requirement + "</td></tr>");
+  }
+}
+loadXMLDoc();
+
+$(document).ready(function(){
+  $("#searchInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#QAtableBody tr").filter(function() {
+      var enable = $(this).text().toLowerCase().indexOf(value) > -1
+      $(this).toggle(enable).toggleClass('d-flex', enable)
+    });
+  });
+});
